919d3b238c9f4a65a68fb0a9a8e8540277704923
skweel <dissingardjino@icloud.com>
2024-10-22 17:30:44 +0200

    mise en place

---
+++ firstappB3/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  firstappB3
+//
+//  Created by Djino Dissingar on 22/10/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ firstappB3/ViewController/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  firstappB3
+//
+//  Created by Djino Dissingar on 22/10/2024.
+//
+
+import Foundation
