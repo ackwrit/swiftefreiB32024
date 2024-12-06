a565e6712e890f5a64dc6c8ccb7b8a6f8115dc5a
skweel <dissingardjino@icloud.com>
2024-12-06 17:17:51 +0100

    mise en place du 6/12/2024

---
+++ firstappB3/View/AllContactView.swift
@@ -0,0 +1,18 @@
+//
+//  AllContactView.swift
+//  firstappB3
+//
+//  Created by Djino Dissingar on 06/12/2024.
+//
+
+import SwiftUI
+
+struct AllContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllContactView()
+}
---
+++ firstappB3/ViewController/AllUserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AllUserViewModel.swift
+//  firstappB3
+//
+//  Created by Djino Dissingar on 06/12/2024.
+//
+
+import Foundation
