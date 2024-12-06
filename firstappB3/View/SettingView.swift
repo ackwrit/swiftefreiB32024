177d16ff28a7167956311a7c8377b3f1c5335a47
skweel <dissingardjino@icloud.com>
2024-12-06 15:54:10 +0100

    bis

---
+++ firstappB3/View/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  firstappB3
+//
+//  Created by Djino Dissingar on 06/12/2024.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
